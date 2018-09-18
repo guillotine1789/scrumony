41900dc5bd6e79fae5ea72c5ee913e79ae25c808
guillotine1789 <serksahin@gmail.com>
2018-09-18 21:23:22 +0300

    Home view with two columns.

--- app/views/Home/Home.js
+++ app/views/Home/Home.js
@@ -7,9 +7,14 @@ export default class Home extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Ceremony name = 'Scrum Poker' imagePath = {require('../../images/ceremonies/scrumPoker/scrumPokerHome.png')} />
-                <Ceremony name = 'Retrospective' imagePath = {require('../../images/ceremonies/retrospective/feedbackHome.png')}/>
-                <Ceremony name = 'Grooming' imagePath = {require('../../images/ceremonies/grooming/shirtsHome.png')}/>
+                <View style={styles.stack}>
+                    <Ceremony name = 'Daily Stand-up' imagePath = {require('../../images/ceremonies/daily/checklist.png')} />
+                    <Ceremony name = 'Scrum Poker' imagePath = {require('../../images/ceremonies/scrumPoker/scrumPokerHome.png')} />
+                </View>
+                <View style={styles.stack}>
+                    <Ceremony name = 'Retrospective' imagePath = {require('../../images/ceremonies/retrospective/feedbackHome.png')}/>
+                    <Ceremony name = 'Grooming' imagePath = {require('../../images/ceremonies/grooming/shirtsHome.png')}/>
+                </View>
             </View>
         );
     }
--- app/views/Home/styles.js
+++ app/views/Home/styles.js
@@ -2,7 +2,13 @@ import {StyleSheet} from "react-native";
 
 export default styles = StyleSheet.create({
     container: {
-        flex : 3,
-        paddingTop: 4,
+        flex: 2,
+        padding: 5,
+        flexDirection: 'column'
+    },
+    stack: {
+        flex: 2,
+        padding: 2,
+        flexDirection: 'row'
     }
 });
